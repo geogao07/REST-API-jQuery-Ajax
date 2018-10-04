310384c6b45283c6814b9e7b9f23120c18ddb8f4
geogao07 <37523228+geogao07@users.noreply.github.com>
2018-10-04 09:39:31 -0400

    Add files via upload
    Frost Glass Effect on IE using CSS SVG

---
+++ FrostedGlass.js
@@ -0,0 +1,58 @@
+
+
+
+
+<style>
+
+
+#s4-workspace {
+  
+  background: url('https://images.unsplash.com/photo-1477346611705-65d1883cee1e?dpr=0.800000011920929&auto=format&fit=crop&w=1199&h=800&q=80&cs=tinysrgb&crop=')  no-repeat;
+  background-size: cover;
+  position: absolute;
+   
+ 
+}
+
+.ms-tableRow{
+	background:inherit;
+	background-size: cover;
+	position: absolute;
+}
+#SVG{
+	background:inherit;
+	background-size: cover;
+	position: absolute;
+}
+
+
+</style>
+<div>
+<svg id="SVG" xmlns="http://www.w3.org/2000/svg" version="1.1" x="400" y="-300" height="400" width="1000">  
+<style>
+    .small { font: italic 13px sans-serif; }
+    .heavy { font: bold 30px sans-serif; }
+
+    /* Note that the color of the text is set with the    *
+     * fill property, the color property is for HTML only */
+    .Rrrrr { font: italic 40px serif; fill: red; }
+  </style>
+ 
+
+ <defs> 
+
+ <filter id="blur">
+ <feGaussianBlur in="SourceGraphic" stdDeviation="50" />   
+ </filter>   
+ </defs>  
+ <image xlink:href="https://www.publicdomainpictures.net/pictures/30000/nahled/plain-white-background.jpg" x="400" y="-300" height="800" width="500" filter="url(#blur)"  style="stroke: none; fill: none;
+               stroke-width: 1px;"/>
+	<text x="400" y="000" class="small">My</text>
+  <text x="420" y="0" class="heavy">cat</text>
+  <text x="435" y="20" class="small">is</text>
+  <text x="445" y="20" class="Rrrrr">Grumpy!</text>		  
+			 
+
+ </svg>
+ 
+<div>
